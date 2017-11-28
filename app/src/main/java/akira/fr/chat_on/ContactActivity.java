0553897691227423b24040c188c3f9260930a859
Quentin Laudrain <akira783@gmail.com>
2017-11-28 16:15:51 +0100

    main

--- app/src/main/java/akira/fr/chat_on/ContactActivity.java
+++ app/src/main/java/akira/fr/chat_on/ContactActivity.java
@@ -1,7 +1,9 @@
 package akira.fr.chat_on;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
 
 public class ContactActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class ContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 }
