00a8b404fc13f9f681c81b8a6147ea37014d29c2
Quentin Laudrain <akira783@gmail.com>
2017-11-30 16:56:18 +0100

    main

--- app/src/main/java/akira/fr/chat_on/ContactActivity.java
+++ app/src/main/java/akira/fr/chat_on/ContactActivity.java
@@ -1,150 +1,35 @@
 package akira.fr.chat_on;
 
-import android.Manifest;
-import android.content.ContentResolver;
-import android.content.ContentUris;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.widget.Toast;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-
 import akira.fr.chat_on.adapter.AllContactsAdapter;
-import akira.fr.chat_on.model.ContactVO;
 
 
 public class ContactActivity extends AppCompatActivity {
-    private String id;
+
     RecyclerView rvContacts;
-    ContactHolder contactHolder;
-    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+        ContactHolder contactHolder = new ContactHolder(this.getApplicationContext());
+        contactHolder.getContactAL();
         rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.READ_CONTACTS)
-                    != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{Manifest.permission.READ_CONTACTS},
-                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-                return;
-            }
-        }
         Toast.makeText(this,"Chargement des contacts", Toast.LENGTH_LONG).show();
-        getAllContacts();
+        AllContactsAdapter contactAdapter = new AllContactsAdapter(contactHolder.getContactAL(), getApplicationContext());
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
+        rvContacts.setAdapter(contactAdapter);
 
 
-        Log.d("Tag","1 - "+ ContactHolder.getInstance().fromTel("0468114628") );
-        Log.d("Tag","2 - "+  ContactHolder.getInstance().fromTel("04 68 11 46 28") );
-        Log.d("Tag","3 - "+  ContactHolder.getInstance().fromTel("04.68.11.46.28") );
 
     }
 
-    private void getAllContacts() {
-        ArrayList<ContactVO> contactVOList = new ArrayList();
-        ContactVO contactVO;
-        Bitmap photo = null;
-        ContentResolver contentResolver = getContentResolver();
-        Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
-        if (cursor.getCount() > 0) {
-            while (cursor.moveToNext()) {
-
-                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
-                if (hasPhoneNumber > 0) {
-                    String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
-                    String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-
-                    contactVO = new ContactVO();
-                    contactVO.setContactName(name);
-
-                    Cursor phoneCursor = contentResolver.query(
-                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                            null,
-                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
-                            new String[]{id},
-                            null);
-                    if (phoneCursor.moveToNext()) {
-                        String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NORMALIZED_NUMBER));
-                        if(phoneNumber == null){
-                            phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                            phoneNumber = phoneNumber.replace(" ","");
-                            phoneNumber=  phoneNumber.replaceFirst("0","+33");
-                        }
-                        contactVO.setContactNumber(phoneNumber);
-                    }
-
-                    phoneCursor.close();
-
-                    Cursor emailCursor = contentResolver.query(
-                            ContactsContract.CommonDataKinds.Email.CONTENT_URI,
-                            null,
-                            ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?",
-                            new String[]{id}, null);
-                    while (emailCursor.moveToNext()) {
-                        String emailId = emailCursor.getString(emailCursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
-                    }
-
-                    try {
-                        InputStream inputStream = ContactsContract.Contacts.openContactPhotoInputStream(getContentResolver(),
-                                ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, new Long(id)));
-
-                        if (inputStream != null) {
-                            photo = BitmapFactory.decodeStream(inputStream);
-                            contactVO.setPhoto(photo);
-                        }else{contactVO.setPhoto( BitmapFactory.decodeResource(getResources(),R.drawable.contact)); }
 
 
-
-                        if(inputStream != null){
-                        inputStream.close();}
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-
-                    contactVOList.add(contactVO);
-                }
-                ContactHolder.getInstance().setContactAL(contactVOList);
-            }
-
-            AllContactsAdapter contactAdapter = new AllContactsAdapter(contactVOList, getApplicationContext());
-            rvContacts.setLayoutManager(new LinearLayoutManager(this));
-            rvContacts.setAdapter(contactAdapter);
-        }
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted,
-
-                } else {
-
-                    // permission denied
-                }
-                return;
-            }
-
-            // other 'switch' lines to check for other
-            // permissions this app might request
-        }
-    }
 
 }
--- app/src/main/java/akira/fr/chat_on/ContactHolder.java
+++ app/src/main/java/akira/fr/chat_on/ContactHolder.java
@@ -1,5 +1,16 @@
 package akira.fr.chat_on;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.ContactsContract;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 import akira.fr.chat_on.model.ContactVO;
@@ -9,22 +20,23 @@ import akira.fr.chat_on.model.ContactVO;
  */
 
 public class ContactHolder {
+    private static Context context;
     private static ArrayList<ContactVO> contactAL;
-    private static ContactHolder INSTANCE = null;
-    private ContactHolder()
-    {}
 
-    public static ContactHolder getInstance()
-    {
-        if (INSTANCE == null)
-        { 	INSTANCE = new ContactHolder();
-        }
-        return INSTANCE;
+
+
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+    public ContactHolder(Context context)
+    {this.setContext(context);
+        getAllContacts();
     }
+
 
     public ArrayList<ContactVO> getContactAL() {
         return contactAL;
     }
+
+    public void setContext(Context context) {this.context = context;}
 
     public void setContactAL(ArrayList<ContactVO> contactAL) {
         this.contactAL = contactAL;
@@ -34,10 +46,9 @@ public class ContactHolder {
         tel=formatNumber(tel);
         for (int ai = 0; ai < contactAL.size(); ai++){
             if(contactAL.get(ai).getContactNumber().equals(tel)){
-               return  contactAL.get(ai).getContactName();
+                return  contactAL.get(ai).getContactName();
             }
         }
-
         return "Unknown contact";
     }
 
@@ -52,6 +63,82 @@ public class ContactHolder {
         }
 
         return num;
+    }
+
+
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted,
+                } else {
+
+                    // permission denied
+                }
+                return;
+            }
+        }
+    }
+
+    private void getAllContacts() {
+        ArrayList<ContactVO> contactVOList = new ArrayList();
+        ContactVO contactVO;
+        Bitmap photo = null;
+        ContentResolver contentResolver = context.getContentResolver();
+        Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
+        if (cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
+                if (hasPhoneNumber > 0) {
+                    String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                    String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                    contactVO = new ContactVO();
+                    contactVO.setContactName(name);
+                    Cursor phoneCursor = contentResolver.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                            new String[]{id},
+                            null);
+                    if (phoneCursor.moveToNext()) {
+                        String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NORMALIZED_NUMBER));
+                        if(phoneNumber == null){
+                            phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                            phoneNumber = phoneNumber.replace(" ","");
+                            phoneNumber=  phoneNumber.replaceFirst("0","+33");
+                        }
+                        contactVO.setContactNumber(phoneNumber);
+                    }
+                    phoneCursor.close();
+                    Cursor emailCursor = contentResolver.query(
+                            ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?",
+                            new String[]{id}, null);
+                    while (emailCursor.moveToNext()) {
+                        String emailId = emailCursor.getString(emailCursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+                    }
+                    try {
+                        InputStream inputStream = ContactsContract.Contacts.openContactPhotoInputStream(context.getContentResolver(),
+                                ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, new Long(id)));
+                        if (inputStream != null) {
+                            photo = BitmapFactory.decodeStream(inputStream);
+                            contactVO.setPhoto(photo);
+                        }else{contactVO.setPhoto( BitmapFactory.decodeResource(context.getResources(),R.drawable.contact)); }
+
+                        if(inputStream != null){
+                            inputStream.close();}
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    contactVOList.add(contactVO);
+                }
+                setContactAL(contactVOList);
+            }
+
+        }
+
     }
 
 }
